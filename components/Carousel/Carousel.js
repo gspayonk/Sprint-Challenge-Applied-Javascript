84a25cfcc699cb35f6149cc0e1a84914995fe3dd
gspayonk <grissobel.castro@gmail.com>
2019-09-06 18:10:30 -0400

    carousel edits

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -29,25 +29,50 @@ const carouselArray = [
 
 const Carousel = arr => {
 
-        const carousel = document.createElement('div');
-        const leftButton = document.createElement('div');
-        const img = document.createElement('img');
-        const rightButton = document.createElement('div');
+    const carousel = document.createElement('div');
+    const leftButton = document.createElement('div');
+    const img = document.createElement('img');
+    const rightButton = document.createElement('div');
 
 
-        carousel.appendChild(leftButton);
-        carousel.appendChild(img);
-        carousel.appendChild(rightButton);
+    carousel.appendChild(leftButton);
+    carousel.appendChild(img);
+    carousel.appendChild(rightButton);
 
 
-        carousel.classList.add('carousel');
-        leftButton.classList.add('left-button');
-        rightButton.classList.add('right-button');
+    carousel.classList.add('carousel');
+    leftButton.classList.add('left-button');
+    rightButton.classList.add('right-button');
 
-        leftButton.textContent = '<';
-        rightButton.textContent = '>';
-        img.src = arr[counter];
+    leftButton.textContent = '<';
+    rightButton.textContent = '>';
+    img.src = arr[counter];
 
-        counter = 0;
-        const pics = items.length;
-        const current = items[0];
+    counter = 0;
+    const pics = items.length;
+    const current = items[0];
+
+    box.classList.add('active');
+
+    function navigate(direction) {
+        current.classList.remove('current');
+        counter = counter + direction;
+        if (direction === -1 &&
+            counter < 0) {
+            counter = pics - 1;
+        }
+        if (direction === 1 &&
+            !items[counter]) {
+            counter = 0;
+        }
+        current = items[counter];
+        current.classList.add('current');
+    }
+    rightButton.addEventListener('click', event => {
+        navigate(1);
+    });
+    leftButton.addEventListener('click', event => {
+        navigate(-1);
+    });
+    navigate(0);
+}();
